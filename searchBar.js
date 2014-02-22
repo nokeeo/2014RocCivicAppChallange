49590269fa62e16c540bb5b5eaf323bb22529303
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-21 23:12:22 -0500

    Added base for searchbar

---
+++ searchBar.js
@@ -0,0 +1,6 @@
+function locationChanged()
+{
+  var locationText = document.getElementById("locationBar").value;
+  //var Geocoder().geocode({address, locationText})
+  Console.Log("Address: " + locationText);
+}
