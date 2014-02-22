7db55183d6ac38a66c093f65a900d9317a7afabf
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-22 00:03:54 -0500

    Removed UpdateHeatMap from index Added markers to address input location

--- searchBar.js
+++ searchBar.js
@@ -5,15 +5,34 @@ function init() {
 function locationChanged()
 {
   var locationText = document.getElementById("locationBar").value;
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<p><b>' + locationText + '</b></p>'+
+      '</div>';
 
   // parse address
-  geocoder.geocode({ 'address': locationText}, function(results, status)
+  geocoder.geocode({'address': locationText}, function(results, status)
   {
     if(status == google.maps.GeocoderStatus.OK)
     {
      map.panTo(results[0].geometry.location); 
     }
+    var marker = new google.maps.Marker({
+      position: results[0].geometry.location,
+      map: map,
+      title:locationText 
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
+
   });
+
+  
   console.log("Address: " + locationText);
   
   //Update HeatMap
