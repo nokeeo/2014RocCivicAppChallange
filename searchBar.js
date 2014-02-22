cf92c51a20d0036f6450dbdc3b643a9e7c0234a7
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-02-21 23:41:37 -0500

    Updated SearchBar.js to be functional

--- searchBar.js
+++ searchBar.js
@@ -1,6 +1,24 @@
+var geocoder;
+function init() {
+  geocoder = new google.maps.Geocoder();
+}
 function locationChanged()
 {
   var locationText = document.getElementById("locationBar").value;
-  //var Geocoder().geocode({address, locationText})
+
+  // parse address
+  geocoder.geocode({ 'address': locationText}, function(results, status)
+  {
+    if(status == google.maps.GeocoderStatus.OK)
+    {
+     map.panTo(results[0].geometry.location); 
+    }
+  });
   console.log("Address: " + locationText);
+  
+  //Update HeatMap
+  heatmap.setMap(null);
+  getData();
+
 }
+init();
