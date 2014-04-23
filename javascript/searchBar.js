e36471479f102c2a4ce762876a89875b457df058
Eric <eric.lee.edl@gmail.com>
2014-04-23 02:12:33 -0400

    Fixed search bar

--- javascript/searchBar.js
+++ javascript/searchBar.js
@@ -1,39 +1,37 @@
-var geocoder;
-function init() {
-  geocoder = new google.maps.Geocoder();
-}
+var searchInfoWindow;
+var searchMarker;
+
 function locationChanged()
 {
-  var locationText = document.getElementById("locationBar").value;
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<p><b>' + locationText + '</b></p>'+
-      '</div>';
+    var locationText = document.getElementById("locationBar").value;
+    var contentString = locationText;
 
-  // parse address
-  geocoder.geocode({'address': locationText}, function(results, status)
-  {
-    if(status == google.maps.GeocoderStatus.OK)
+    geocoder = new google.maps.Geocoder();
+    
+    if(searchMarker)
+        searchMarker.setMap(null);
+
+    // parse address
+    geocoder.geocode({'address': locationText}, function(results, status)
     {
-     map.panTo(results[0].geometry.location); 
-    }
-    var marker = new google.maps.Marker({
-      position: results[0].geometry.location,
-      map: map,
-      title:locationText 
-    });
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
-    });
+        if(status == google.maps.GeocoderStatus.OK) {
+            googleMap.panTo(results[0].geometry.location); 
+        }
+        
+        searchMarker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: googleMap,
+            title:locationText 
+        });
+        
+        searchInfowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        
+        google.maps.event.addListener(searchMarker, 'click', function() {
+            searchInfowindow.open(googleMap, searchMarker);
+        });
 
-  });
-
-  
-  console.log("Address: " + locationText);
+    });
 
 }
-init();
